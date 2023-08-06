778193ee1964ec82f3a55e1ef47e574712399d80
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-08-06 23:03:10 +0700

    add some nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/navbar/navbar';
 
 function App() {
+  const topic = ["Home", "About", "Contact"];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <Navbar topics={topic} />
       </header>
     </div>
   );
